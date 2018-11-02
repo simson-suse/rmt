548be9af1e4f9f56725d737a0fef944bfb4478e1
Simon Schricker <sschricker@suse.de>
2018-11-02 15:25:40 +0100

    verification of static completion + refactor

--- lib/rmt/cli/completion.rb
+++ lib/rmt/cli/completion.rb
@@ -2,8 +2,6 @@ class RMT::CLI::Completion
   @cli_words = []
   @current_word = ''
   @previous_word = ''
-  @static_options = []
-  @completions = []
 
   def initialize
     split_cli_feed
@@ -22,6 +20,21 @@ class RMT::CLI::Completion
     @cli_words.join == @cli_words.join.downcase
   end
 
+  def static_completion_possible?(index = 1)
+    if index > 3
+      return false
+    elsif index >= @cli_words.length - 1
+      return true
+    else
+      word_to_verify = @cli_words[index]
+      previous_word = @cli_words[index - 1]
+      if generate_static_options(previous_word).include? word_to_verify
+        return static_completion_possible?(index + 1)
+      end
+    end
+    return false
+  end
+
   def determine_current_word
     @current_word = @cli_words.last
   end
@@ -34,38 +47,42 @@ class RMT::CLI::Completion
     end
   end
 
-  def generate_static_options
-    submodule = @previous_word.slice(0,1).capitalize + @previous_word.slice(1, @previous_word.length).downcase
+  def generate_static_options(previous_word)
+    submodule = previous_word.slice(0,1).capitalize + previous_word.slice(1, previous_word.length).downcase
     options = []
 
     # exceptions:
-    if @previous_word == 'rmt-cli' || @previous_word =='help' then submodule = 'Main' end
-    if @previous_word == 'repo' then submodule = 'Repos' end
-    if @previous_word == 'product' then submodule = 'Products' end
-    if @previous_word == 'rmt-cli' then options.append('help') end
+    if previous_word == 'rmt-cli' || previous_word =='help' then submodule = 'Main' end
+    if previous_word == 'repo' then submodule = 'Repos' end
+    if previous_word == 'product' then submodule = 'Products' end
+    if previous_word == 'custom' then submodule = 'ReposCustom' end
+    if previous_word == 'rmt-cli' then options.append('help') end
 
     begin
       options.concat RMT::CLI.module_eval(submodule).commands.keys
     rescue NameError
     end
 
-    @static_options = options
+    return options
   end
 
   def generate_completions
     completions = []
+    static_options = generate_static_options(@previous_word)
 
-    @static_options.each do |option|
+    static_options.each do |option|
       if option.start_with?(@current_word)
         completions.append(option)
       end
     end
 
-    @completions = completions
+    return completions
   end
 
   def complete
-    print @completions.join("\n")
+    completions = generate_completions
+
+    print completions.join("\n")
   end
 
 end
